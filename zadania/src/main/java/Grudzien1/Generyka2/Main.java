2ff72a9791f7e2d0890a5d9081ee185693d39916
Karol011 <karollacek1@gmail.com>
2019-12-02 17:07:58 +0100

    test edycji plikow w gicie

--- zadania/src/main/java/Grudzien1/Generyka2/Main.java
+++ zadania/src/main/java/Grudzien1/Generyka2/Main.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         MyList<MyList> myList = new MyList<>(5);
+        System.out.println();
 
         //myList.addElement(5);
 
